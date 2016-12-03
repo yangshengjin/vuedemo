7af4844d7d776b352d61ccde1de552670964993d
yangsj <841860145@qq.com>
2016-12-03 18:42:48 +0800

    learn vue

---
+++ src/App.vue
@@ -0,0 +1,16 @@
+<template>
+  <div>
+      <router-view class="view" keep-alive transition transition-mode="out-in">
+      </router-view>
+  </div>
+</template>
+
+<script type="text/javascript">
+  export default {
+      components: {}
+  }
+</script>
+
+<style lang="scss">
+    @import "./style/style";
+</style>
---
+++ src/page/user/index.vue
@@ -0,0 +1,4 @@
+
+<template>
+  <div>user index page</div>
+</template>
